9317e67380bc5a15d9d978468a5fbe6953fb9b5d
hamza kılıç <hamza2358@hotmail.com>
2023-11-15 14:55:23 +0300

    added abstract and concrete classes in business

---
+++ Layer/BusinessLayer/Concrete/ProductManager.cs
@@ -0,0 +1,45 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using DataAccessLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+        public void TAdd(Product t)
+        {
+            _productDal.Insert(t);
+        }
+
+        public void TDelete(Product t)
+        {
+            _productDal.Delete(t);
+        }
+
+        public Product TGetById(int id)
+        {
+            return _productDal.GetById(x => x.ProductId == id);
+        }
+
+        public List<Product> TGetList()
+        {
+            return _productDal.GetAll();
+        }
+
+        public void TUpdate(Product t)
+        {
+            _productDal.Update(t);
+        }
+    }
+}
